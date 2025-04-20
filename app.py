eb09ee4b188c1d948ac335858106208cc0861947
kasiaa4477 <96470072+kasiaa4477@users.noreply.github.com>
2025-04-20 21:12:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def predict():
         return jsonify({"error": "Podano nieprawidłowe wartości numeryczne"}), 400
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run()
